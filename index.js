fcd5ffd248f658a6c06cb7b2379bd6e6a9a6cd7c
git lysing098 <lyzing1021@gmail.com>
2025-09-19 22:53:19 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const fs = require('fs');
-require('dotenv').config();
+
 
 const productRouter = require('./router/productRouter');
 
